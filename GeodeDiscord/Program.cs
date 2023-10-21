2cf7c45405468928ff3ea5a201fc476bbfb9e9c4
ConfiG <cgytrus@cgyt.ru>
2023-10-22 02:43:53 +0300

    embed response to reaction

--- GeodeDiscord/Program.cs
+++ GeodeDiscord/Program.cs
@@ -1,11 +1,17 @@
 ﻿using Discord;
+using Discord.Interactions;
 using Discord.WebSocket;
 
 namespace GeodeDiscord;
 
 public class Program {
     private readonly DiscordSocketClient _client = new(new DiscordSocketConfig {
-        GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
+        GatewayIntents =
+            GatewayIntents.GuildMessageReactions |
+            GatewayIntents.GuildIntegrations |
+            GatewayIntents.GuildEmojis |
+            GatewayIntents.GuildMessages |
+            GatewayIntents.MessageContent
     });
 
     private static void Main(string[] args) => new Program()
@@ -16,8 +22,7 @@ public class Program {
     private async Task MainAsync() {
         _client.Log += LogAsync;
         _client.Ready += ReadyAsync;
-        _client.MessageReceived += MessageReceivedAsync;
-        _client.InteractionCreated += InteractionCreatedAsync;
+        _client.ReactionAdded += ReactionAddedAsync;
 
         await _client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("DISCORD_TOKEN"));
         await _client.StartAsync();
@@ -34,23 +39,44 @@ public class Program {
         return Task.CompletedTask;
     }
 
-    private async Task MessageReceivedAsync(SocketMessage message) {
-        if (message.Author.Id == _client.CurrentUser.Id)
+    private async Task ReactionAddedAsync(Cacheable<IUserMessage, ulong> cachedMessage,
+        Cacheable<IMessageChannel, ulong> cachedChannel, SocketReaction reaction) {
+        if (reaction.UserId == _client.CurrentUser.Id)
             return;
 
-        if (message.Content == "!ping") {
-            ComponentBuilder? cb = new ComponentBuilder()
-                .WithButton("Click me!", "unique-id");
-            await message.Channel.SendMessageAsync("pong!", components: cb.Build());
-        }
+        // 💬
+        if (reaction.Emote.Name != "\ud83d\udcac")
+            return;
+
+        IUserMessage message = await cachedMessage.GetOrDownloadAsync();
+        if (message.Author.IsBot || message.Author.IsWebhook)
+            return;
+
+        IMessageChannel channel = await cachedChannel.GetOrDownloadAsync();
+        await channel.SendMessageAsync(
+            allowedMentions: AllowedMentions.None,
+            embeds: MessageToEmbeds(message).ToArray()
+        );
     }
 
-    private static async Task InteractionCreatedAsync(SocketInteraction interaction) {
-        if (interaction is not SocketMessageComponent component)
-            return;
-        if (component.Data.CustomId == "unique-id")
-            await interaction.RespondAsync("Thank you for clicking my button!");
-        else
-            Console.WriteLine("An ID has been received that has no handler!");
+    private static IEnumerable<Embed> MessageToEmbeds(IMessage message) {
+        string authorName = message.Author.GlobalName ?? (message.Author.DiscriminatorValue == 0 ?
+            message.Author.Username : $"{message.Author.Username}{message.Author.Discriminator}");
+        string authorIcon = message.Author.GetAvatarUrl() ?? message.Author.GetDefaultAvatarUrl();
+
+        List<IAttachment> embeddableAttachments =
+            message.Attachments.Where(att =>
+                !att.IsSpoiler() && att.ContentType.StartsWith("image/", StringComparison.Ordinal)).ToList();
+
+        yield return new EmbedBuilder()
+            .WithAuthor(authorName, authorIcon)
+            .WithDescription($"{message.Content}\n\n{message.GetJumpUrl()}")
+            .WithImageUrl(embeddableAttachments.Count > 0 ? embeddableAttachments[0].Url : null)
+            .Build();
+
+        foreach (IAttachment attachment in embeddableAttachments.Skip(1).Take(9))
+            yield return new EmbedBuilder()
+                .WithImageUrl(attachment.Url)
+                .Build();
     }
 }
