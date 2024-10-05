4e938ab8b28dbd2d640b27ea8ddb931bb72e1518
ConfiG <cgytrus@cgyt.ru>
2024-10-05 20:51:48 +0300

    configurable db path

--- GeodeDiscord/Database/ApplicationDbContext.cs
+++ GeodeDiscord/Database/ApplicationDbContext.cs
@@ -7,7 +7,7 @@ namespace GeodeDiscord.Database;
 public class ApplicationDbContext : DbContext {
     public DbSet<Quote> quotes { get; set; } = null!;
 
-    public string dbPath { get; } =
+    public string dbPath { get; } = Environment.GetEnvironmentVariable("GEODE_BOT_DB_PATH") ??
         Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "GeodeDiscord.db");
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
