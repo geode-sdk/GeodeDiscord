8867d0841c12ab1485060eb5b284e70b90734ab1
ConfiG <cgytrus@cgyt.ru>
2024-10-06 00:09:32 +0300

    sticky stuff

--- GeodeDiscord/Modules/StickyModule.cs
+++ GeodeDiscord/Modules/StickyModule.cs
@@ -15,11 +15,12 @@ using Serilog;
 
 namespace GeodeDiscord.Modules;
 
-[Group("sticky", "Give roles that save after relogging the server."), DefaultMemberPermissions(GuildPermission.ManageRoles), EnabledInDm(false), UsedImplicitly]
+[Group("sticky", "Give roles that save after rejoining the server."), DefaultMemberPermissions(GuildPermission.ManageRoles), CommandContextType(InteractionContextType.Guild), UsedImplicitly]
 public class StickyModule(ApplicationDbContext db) : InteractionModuleBase<SocketInteractionContext> {
     public static Task OnUserJoined(SocketGuildUser user, ApplicationDbContext db) {
         List<StickyRole> roles = db.stickyRoles.Where(sr => sr.userId == user.Id).ToList();
-        if (roles.Count == 0) return Task.CompletedTask;
+        if (roles.Count == 0)
+            return Task.CompletedTask;
 
         Log.Information("Restoring sticky roles for {User}", user);
         foreach (StickyRole role in roles) {
@@ -28,14 +29,16 @@ public class StickyModule(ApplicationDbContext db) : InteractionModuleBase<Socke
                 continue;
             }
             try { user.AddRoleAsync(r); }
-            catch (Exception ex) { Log.Error(ex, "Failed to add role to user"); }
+            catch (Exception ex) {
+                Log.Error(ex, "Failed to add role to user");
+            }
         }
 
         return Task.CompletedTask;
     }
 
-    [SlashCommand("add", "Add a sticky role to the user."), UsedImplicitly]
-    public async Task Add([Autocomplete(typeof(RoleAutocompleteHandler))] string role, 
+    [SlashCommand("add", "Adds a sticky role to the user."), UsedImplicitly]
+    public async Task Add([Autocomplete(typeof(RoleAutocompleteHandler))] string role,
         [Summary(null, "User to add the role to.")] IUser user) {
         if (!ulong.TryParse(role, out ulong roleId)) {
             await RespondAsync("❌ Role is invalid!", ephemeral: true);
@@ -53,9 +56,8 @@ public class StickyModule(ApplicationDbContext db) : InteractionModuleBase<Socke
         }
 
         if (user is IGuildUser guildUser) {
-            try { 
-                await guildUser.AddRoleAsync(r);
-            } catch (Exception ex) {
+            try { await guildUser.AddRoleAsync(r); }
+            catch (Exception ex) {
                 Log.Error(ex, "Failed to add role to user");
                 await RespondAsync($"❌ Failed to add role to user:\n{ex.Message}", ephemeral: true);
                 return;
@@ -64,8 +66,7 @@ public class StickyModule(ApplicationDbContext db) : InteractionModuleBase<Socke
 
         db.stickyRoles.Add(new StickyRole { userId = user.Id, roleId = roleId });
         try { await db.SaveChangesAsync(); }
-        catch (Exception ex)
-        {
+        catch (Exception ex) {
             Log.Error(ex, "Failed to save sticky role");
             await RespondAsync("❌ Failed to save sticky role!", ephemeral: true);
             return;
@@ -97,7 +98,7 @@ public class StickyModule(ApplicationDbContext db) : InteractionModuleBase<Socke
     }
 
     [SlashCommand("remove", "Remove a sticky role from the user."), UsedImplicitly]
-    public async Task Remove([Autocomplete(typeof(RoleAutocompleteHandler))] string role, 
+    public async Task Remove([Autocomplete(typeof(RoleAutocompleteHandler))] string role,
         [Summary(null, "User to remove the role from.")] IUser user) {
         if (!ulong.TryParse(role, out ulong roleId)) {
             await RespondAsync("❌ Role is invalid!", ephemeral: true);
@@ -134,15 +135,19 @@ public class StickyModule(ApplicationDbContext db) : InteractionModuleBase<Socke
             return;
         }
 
-        await RespondAsync($"✅ Successfully removed sticky role **{r.Name}** from {user.Mention}!");
+        await RespondAsync($"✅ Successfully removed sticky role *{r.Name}* from {user.Mention}!");
     }
 
-    public class RoleAutocompleteHandler : AutocompleteHandler {
+    private class RoleAutocompleteHandler : AutocompleteHandler {
         public override Task<AutocompletionResult> GenerateSuggestionsAsync(IInteractionContext context,
             IAutocompleteInteraction autocompleteInteraction, IParameterInfo parameter, IServiceProvider services) {
+            string value = autocompleteInteraction.Data.Current.Value as string ?? string.Empty;
+            IRole everyoneRole = context.Guild.EveryoneRole;
             try {
                 return Task.FromResult(AutocompletionResult.FromSuccess(context.Guild.Roles
-                    .Where(r => r.Name.Contains(autocompleteInteraction.Data.Current.Value as string ?? string.Empty))
+                    .Where(r =>
+                        r != everyoneRole &&
+                        EF.Functions.Like(r.Name, $"%{value}%"))
                     .Take(25)
                     .Select(r => new AutocompleteResult(r.Name, r.Id.ToString()))));
             }
