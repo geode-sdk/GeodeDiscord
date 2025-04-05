e3f2a6796371ff44c3706045dd08cf443e73cb04
ConfiG <cgytrus@cgyt.ru>
2025-04-05 16:15:31 +0300

    who did this

--- GeodeDiscord/Database/Entities/StickyRole.cs
+++ GeodeDiscord/Database/Entities/StickyRole.cs
@@ -6,8 +6,7 @@ using Microsoft.EntityFrameworkCore;
 namespace GeodeDiscord.Database.Entities;
 
 [Index(nameof(userId)), Index(nameof(roleId))]
-public record StickyRole
-{
+public record StickyRole {
     [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
     public required ulong userId { get; init; }
     public required ulong roleId { get; init; }
