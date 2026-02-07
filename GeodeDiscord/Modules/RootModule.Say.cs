7dbbe4d490654a5db59ba5234b2c16f40487c8e6
ConfiG <serious@cwonfig.pet>
2026-02-07 16:47:33 +0300

    add /say

---
+++ GeodeDiscord/Modules/RootModule.Say.cs
@@ -0,0 +1,14 @@
+﻿using Discord;
+using Discord.Interactions;
+
+using JetBrains.Annotations;
+
+namespace GeodeDiscord.Modules;
+
+public partial class RootModule {
+    [SlashCommand("say", "Make the bot say something as you."),
+     CommandContextType(InteractionContextType.Guild),
+     DefaultMemberPermissions(GuildPermission.Administrator),
+     UsedImplicitly]
+    public async Task Say(string message) => await RespondAsync($"`@{Context.User.GlobalName}`: {message}");
+}
